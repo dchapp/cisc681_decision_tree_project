b7cb61b6b7959e3eb06c3bf53fded70484e12b2a
dchapp <dchapp@udel.edu>
2016-05-13 14:22:49 -0400

    cleanup

--- src/DecisionTreeNode.py
+++
@@ -1,6 +0,0 @@
-class DecisionTreeNode(object):
-    def __init__(self, indices, feature, children):
-        self.indices = indices
-        self.feature = feature
-        self.children = children
-        self.klass = None
